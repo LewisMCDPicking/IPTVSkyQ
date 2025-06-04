107076c0c251feb965482f1127f203ce548a5b35
LewisMCDPicking <lewis.atkins01@gmail.com>
2025-06-05 00:51:40 +0100

    Update script.js.js

--- script.js.js
+++ script.js.js
@@ -1,6 +1,21 @@
 const video = document.getElementById('videoPlayer');
+const preview = document.getElementById('previewPlayer');
 
-function playChannel(url) {
-  video.src = url;
-  video.play();
+let currentPreviewUrl = '';
+let currentMainUrl = '';
+
+function channelClicked(url) {
+  if (currentPreviewUrl !== url) {
+    // First press: just preview
+    preview.src = url;
+    preview.play();
+    currentPreviewUrl = url;
+  } else {
+    // Second press: play on main player
+    video.src = url;
+    video.play();
+    preview.src = '';
+    currentMainUrl = url;
+    currentPreviewUrl = '';
+  }
 }
